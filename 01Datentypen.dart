e1c6c99a8c5eedafde4b197b559d8f6f7d82610c
YannikSchreiter <68228389+YannikSchreiter@users.noreply.github.com>
2020-07-13 14:49:24 +0200

    Create 01Datentypen.dart

---
+++ 01Datentypen.dart
@@ -0,0 +1,15 @@
+main(){
+  int a = 1;
+  int b = 2;
+  print(a);
+    print(b);
+  
+  double c = 0.1;
+  double d = 0.2;
+  print(c);
+    print(d);
+  
+  String e = "abcde";
+  print(e);
+    
+}
